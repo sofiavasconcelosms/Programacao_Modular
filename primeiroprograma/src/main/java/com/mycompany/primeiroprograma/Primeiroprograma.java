6d1ecb3b073535ac7e17695110641df58a1d0479
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-08-21 20:24:53 -0300

    Add files via upload

---
+++ primeiroprograma/src/main/java/com/mycompany/primeiroprograma/Primeiroprograma.java
@@ -0,0 +1,10 @@
+package com.mycompany.primeiroprograma;
+
+public class Primeiroprograma {//classe primeiro programa que faz parte do pacote primeiro programa
+    public static void main(String[] args){//metodo principal que esta dentro de uma classe
+        System.out.print("Ola mundo!");
+        System.out.println("Meu primeiro projeto java");
+    }
+}
+
+
